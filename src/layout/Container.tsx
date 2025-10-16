91d0f46af0b0e6345659f707e4ed37ac72d954ba
yewon <970won@gmail.com>
2025-10-16 18:52:07 +0900

    dist13

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled';
+import borderImage from '/background.png';
 
 const Container = styled.div`
   border: 30px solid transparent; /* 테두리의 초기 값 설정 */
-  border-image-source: url('/background.png'); /* 이미지 경로 설정 */
+  border-image-source: url(${borderImage}); /* 이미지 경로 설정 */
   border-image-slice: 30% 49%; /* 이미지의 크기 설정 */
   border-image-width: 280px; /* 테두리 이미지의 너비 설정 */
   background-color: #fcf8f0;
