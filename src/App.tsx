0d37c4fcf16d96b865cd9d4d6a9011b81901f1ca
yewon <970won@gmail.com>
2025-11-01 20:14:25 +0900

    fidy_5

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
-import { Heading1, Heading12} from '@/components/Text';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Heading1, Heading12 } from '@/components/Text';
 import Wrapper from '@/components/Wrapper';
 import Account from '@/layout/Account/Account';
 import Container from '@/layout/Container';
@@ -27,48 +28,52 @@ function App() {
       const { offsetTop } = galleryRef.current;
       const scrollPosition = window.scrollY;
 
-      if (scrollPosition >= offsetTop) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
+      setIsVisible(scrollPosition >= offsetTop);
     }
   };
 
   return (
-    <Container>
-      <Wrapper>
-        <Main />
-      </Wrapper>
-      <Wrapper>
-        <Heading1></Heading1>
-        <Invitation />
-      </Wrapper>
-      <Wrapper ref={galleryRef}>
-        <Heading12>Gallery</Heading12>
-        <GalleryWrap />
-      </Wrapper>
-      <Wrapper>
-        <Heading12>Location</Heading12>
-        <Location />
-      </Wrapper>
-       
-      <Wrapper>
-        <Heading12>신랑 신부에게</Heading12>
-        <Guestbook />
-      </Wrapper>
-     <Wrapper>
-        <Heading12>마음 전하실 곳</Heading12>
-        <Account />
-      </Wrapper>
-      
-       <Wrapper>
-        <Bottom/>
-      </Wrapper>
-      <FloatingBar isVisible={isVisible} />
-      
-      
-    </Container>
+    <HelmetProvider>
+      <Helmet>
+        <title>이규승 🖤 신예원 결혼합니다</title>
+        <meta name="description" content="2026년 8월 29일 (토) 12:30 GRC 1층 아산홀" />
+        <meta property="og:url" content="https://dpajddl.github.io/invitation/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="이규승 🖤 신예원 결혼합니다" />
+        <meta property="og:description" content="2026년 8월 29일 (토) 12:30 GRC 1층 아산홀" />
+        <meta property="og:image" content="https://dpajddl.github.io/invitation/thumbnail.jpg" />
+      </Helmet>
+
+      <Container>
+        <Wrapper>
+          <Main />
+        </Wrapper>
+        <Wrapper>
+          <Heading1 />
+          <Invitation />
+        </Wrapper>
+        <Wrapper ref={galleryRef}>
+          <Heading12>Gallery</Heading12>
+          <GalleryWrap />
+        </Wrapper>
+        <Wrapper>
+          <Heading12>Location</Heading12>
+          <Location />
+        </Wrapper>
+        <Wrapper>
+          <Heading12>신랑 신부에게</Heading12>
+          <Guestbook />
+        </Wrapper>
+        <Wrapper>
+          <Heading12>마음 전하실 곳</Heading12>
+          <Account />
+        </Wrapper>
+        <Wrapper>
+          <Bottom />
+        </Wrapper>
+        <FloatingBar isVisible={isVisible} />
+      </Container>
+    </HelmetProvider>
   );
 }
 
